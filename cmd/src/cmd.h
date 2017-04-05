802f06061752af8816946eaba879d137c6c018c5
Michael <bRanger82@users.noreply.github.com>
2017-04-05 08:00:02 +0200

    Update cmd.h
    added comments

--- cmd/src/cmd.h
+++ cmd/src/cmd.h
@@ -1,20 +1,25 @@
-const char * cmdExit = "exit";
-const char * cmdCurrDir = "pwd";
-const char * cmdFHSDate = "fhsdate";
-const char * cmdFHSTime = "fhstime";
-const char * cmdHelp = "help";
-const char * cmdChangeDir = "cd";
+// Definition der Kommandos welche interpretiert werden koennen
+const char * cmdExit = "exit";       // Shell soll beendet werden
+const char * cmdCurrDir = "pwd";     // Ausgabe aktuelles Verzeichnis auf stdout
+const char * cmdFHSDate = "fhsdate"; // Ausgabe aktuelles Datum auf stdout
+const char * cmdFHSTime = "fhstime"; // Ausgabe aktuelle Uhrzeit auf stdout
+const char * cmdHelp = "help";       // Ausgabe der Hilfe-Information auf stdout
+const char * cmdChangeDir = "cd";    // Aendern des aktuellen Verzeichnisses
+// Ende Definition Kommandos
 
-#define RET_PARSE_CMD_SYS -12
-#define RET_PARSE_CMD_NOT_SET -5
-#define RET_PARSE_CMD_ERROR -2
-#define RET_PARSE_CMD_UNKNOWN -1
-#define RET_PARSE_CMD_OK 0
-#define RET_PARSE_CMD_EXIT 1
+// Definition der Rueckgabe-Werte der Parsing Funktion
+#define RET_PARSE_CMD_SYS -12    // Aufruf von system(command)
+#define RET_PARSE_CMD_NOT_SET -5 // Eingabe enthaelt nur Wert(e) von DELIMS
+#define RET_PARSE_CMD_ERROR -2   // Fehler beim Lesen der Eingabe (falscher Parameter)
+#define RET_PARSE_CMD_UNKNOWN -1 // Eingabe unbekannt
+#define RET_PARSE_CMD_OK 0       // Eingabe konnte erfolgreich verarbeitet werden
+#define RET_PARSE_CMD_EXIT 1     // Der Befehl zum Beenden der Shell
+// Ende Definition Rueckgabe-Werte
 
-#define MAX_LENGTH 1024
-#define DELIMS " \t\r\n"
+#define MAX_LENGTH 1024  // Puffer fuer die Eingabe
+#define DELIMS " \t\r\n" // Trennzeichen-Erkennung zwischen den Parametern
 
+// Methode, welche das aktuelle Datum und Uhrzeit zurueck liefert
 struct tm * getTimeInfo(void)
 {
     time_t rawtime;
@@ -22,5 +27,5 @@ struct tm * getTimeInfo(void)
     return localtime ( &rawtime );
 }
 
-void showHelp(void);
-int parseInput(char * input);
+void showHelp(void);          // Methode, welche die Hilfe auf stdout ausgibt
+int parseInput(char * input); // Methode, welche die Eingabe verarbeitet. Rueckgabewerte siehe RET_PARSE_CMD_*
