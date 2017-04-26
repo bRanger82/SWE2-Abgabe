591087c9dd93bd4a7838f1bba37f70a831966d5b
Michael <bRanger82@users.noreply.github.com>
2017-04-26 14:26:33 +0200

    Update Rekursion.c

--- Rekursion/Rekursion.c
+++ Rekursion/Rekursion.c
@@ -11,19 +11,19 @@ int rekursiv(int i)
     return i;
 }
 
-int rekSum(int i, int min)
+int rekSumMin(int i, int min)
 {
     if (i < min)
         return 0;
     else
-        return i + rekSum(i-1, min);
+        return i + rekSumMin(i-1, min);
 }
 
-int rekSum2(int i, int max)
+int rekSumMax(int i, int max)
 {
     if (i <= max)
     {
-        return i + rekSum2(i + 1, max);
+        return i + rekSumMax(i + 1, max);
     } else
     {
         return 0;
@@ -36,9 +36,9 @@ int main(int argc, char* argv[])
     printf("Ausgabe Funktion Rekursion:\n");
     rekursiv(5);
     printf("\n");
-    printf("Summe (Rekursion): %d\n", rekSum(10, 5));
+    printf("Summe (Rekursion - rekSumMin): %d\n", rekSumMin(10, 5));
     printf("\n");
-    printf("Summe (Rekursion): %d\n", rekSum2(5, 10));
+    printf("Summe (Rekursion - rekSumMax): %d\n", rekSumMax(5, 10));
     printf("\n");
     return EXIT_SUCCESS;
 }
